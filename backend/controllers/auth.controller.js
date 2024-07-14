e9e39ae594993f94057274fd3c01778145667bd1
DvHuy <ahuyahihi4@gmail.com>
2024-07-14 21:54:07 +0700

    GenerateJWT, Login endpoint, Logout endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -32,6 +33,8 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       //Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
+
       await newUser.save();
 
       res.status(201).json({
@@ -51,10 +54,38 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  console.log("Login User");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    console.log("Error in login controller", err.message);
+    res.status(500).json({ error: "Interval Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  console.log("Login User");
+  try {
+    res.cookie("jwt", "", {maxAge: 0})
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (err) {
+    console.log("Error in logout controller", err.message);
+    res.status(500).json({ error: "Interval Server Error" });
+  }
 };
