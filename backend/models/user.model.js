0b3049f3e658c19c13f171927dd1de4e3561e23e
DvHuy <ahuyahihi4@gmail.com>
2024-07-16 22:27:47 +0700

    sendMessage, getMessages function in message controller

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,30 +1,35 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-    required: true,
-  },
-  profilePic: {
-    type: String,
-    default: "",
-  },
-});
+
+  //dùng để ghi lại thời gian tạo tài khoản: createAt, updateAt
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -10,8 +12,11 @@ const PORT = process.env.PORT || 5000
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
+app.use(cookieParser())
+
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, ()=>{
     connectToMongoDB();
