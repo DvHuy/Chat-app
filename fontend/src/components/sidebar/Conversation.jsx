910e119fcc685757a22416fd68d6a96c5d01d38c
DvHuy <ahuyahihi4@gmail.com>
2024-07-19 22:58:47 +0700

    Login, SignUp UI

---
+++ fontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Conversation = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img
+              src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl">🎃</span>
+          </div>
+        </div>
+      </div>
+      <div className="divider my-0 py-0 h-1"></div>
+    </>
+  );
+};
+
+export default Conversation;
