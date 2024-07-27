f6f0815a93f480f40b940b564179e3c7b22cace7
DvHuy <ahuyahihi4@gmail.com>
2024-07-27 08:51:58 +0700

    Creat AuthContext, Login & Logout functionality

--- fontend/src/hooks/useSignup.js
+++ fontend/src/hooks/useSignup.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
-import toast from "react-hot-toast"
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
   const signup = async ({
     fullName,
     username,
@@ -17,49 +21,52 @@ const useSignup = () => {
       gender,
     });
     if (!success) return;
+
     setLoading(true);
     try {
-        const res = await fetch("/api/auth/signup",{
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              fullName,
-              username,
-              password,
-              confirmPassword,
-              gender,
-            })
-        })
-        const data = await res.json();
-        console.log(data);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
     } catch (error) {
       toast.error(error.message);
-    
     } finally {
       setLoading(false);
     }
   };
-  return {loading, signup}
-};
 
+  return { loading, signup };
+};
 export default useSignup;
 
-const handleInputErrors = ({
+function handleInputErrors({
   fullName,
   username,
   password,
   confirmPassword,
   gender,
-}) => {
+}) {
   if (!fullName || !username || !password || !confirmPassword || !gender) {
     toast.error("Please fill in all fields");
     return false;
   }
 
   if (password !== confirmPassword) {
-    toast.error("Password do not match");
+    toast.error("Passwords do not match");
     return false;
   }
 
@@ -67,5 +74,6 @@ const handleInputErrors = ({
     toast.error("Password must be at least 6 characters");
     return false;
   }
+
   return true;
-};
+}
