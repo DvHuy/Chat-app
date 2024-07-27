a6a3e8626d8478ab7d0de55efc0f6edaa7786546
DvHuy <ahuyahihi4@gmail.com>
2024-07-27 10:37:47 +0700

    Get Conversation, SendMessage Functionality

--- fontend/src/components/sidebar/Conversations.jsx
+++ fontend/src/components/sidebar/Conversations.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Conversation from './Conversation'
+import React from "react";
+import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
-  return (
-    <div className="py-2 flex flex-col overflow-hidden">
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-    </div>
-  )
-}
+  const { loading, conversations } = useGetConversations();
 
-export default Conversations
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          lastIdx={idx === conversations.length - 1}
+          emoji={getRandomEmoji()}
+        />
+      ))}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+    </div>
+  );
+};
+
+export default Conversations;
